741dd668dc667cb0336f97cb16be8510c20680ad
Maycool <maycoolvinicius@hotmail.com>
2026-08-11 23:56:32 -0300

    first commit

---
+++ src/main/java/com/maycool/docker_manager/config/DockerClientConfig.java
@@ -0,0 +1,21 @@
+package com.maycool.docker_manager.config;
+
+import com.github.dockerjava.api.DockerClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DockerClientConfig {
+
+
+    @Value("${docker.socket.path}")
+    private String dockerSocketPath;
+
+    @Bean
+    public DockerClient buildDockerClient() {
+        return null;
+    }
+
+}
+
