dbaadea5ec3cbf4f5babbbdc77a3785385af8891
Maycool <maycoolvinicius@hotmail.com>
2026-08-12 23:29:53 -0300

    feat: add life cycle management

--- src/main/java/com/maycool/docker_manager/config/DockerClientConfig.java
+++ src/main/java/com/maycool/docker_manager/config/DockerClientConfig.java
@@ -1,6 +1,9 @@
 package com.maycool.docker_manager.config;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +17,23 @@ public class DockerClientConfig {
 
     @Bean
     public DockerClient buildDockerClient() {
-        return null;
+        DefaultDockerClientConfig.Builder dockerClientConfigBuilder = DefaultDockerClientConfig
+                .createDefaultConfigBuilder();
+
+        if(this.dockerSocketPath != null && this.dockerSocketPath.startsWith("unix://")) {
+            dockerClientConfigBuilder.withDockerHost(dockerSocketPath)
+                    .withDockerTlsVerify(false);
+        }
+
+        DefaultDockerClientConfig dockerClientConfig = dockerClientConfigBuilder
+                .build();
+
+        ApacheDockerHttpClient dockerHttpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(dockerClientConfig.getDockerHost()).build();
+
+        return DockerClientBuilder.getInstance(dockerClientConfig)
+                .withDockerHttpClient(dockerHttpClient)
+                .build();
     }
 
 }
